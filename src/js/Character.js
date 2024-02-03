8878fd6150e98cf8cfe0bc474be50f2c7c1a8bb9
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-02-03 17:17:39 +0600

    докачал загузчики кроме core.js

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,5 +1,10 @@
-class Character{
-    constructor(){
-        
-    }
+export default class Character {
+  constructor(name, type, health, level, attack, defence) {
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+    this.attack = attack;
+    this.defence = defence;
+  }
 }
---
+++ src/js/_tests_/Character-test.js
@@ -0,0 +1,16 @@
+import Character from "../Character";
+const type_players = [
+  "Bowman",
+  "Swordsman",
+  "Magician",
+  "Daemon",
+  "Undead",
+  "Zombie",
+];
+const player = new Character("Niki", "Bowman");
+console.log(type_players);
+
+
+
+
+//?         npm i -D core-js@^3
