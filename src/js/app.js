368afabc2204b8bba567306ba185f37776a86603
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-02-04 19:46:26 +0600

    исправил app.js и gitignore

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import Character from './Character';
 import Bowerman from './Bowerman';
 import Daemon from './Daemon';
@@ -5,3 +6,4 @@ import Magician from './Magician';
 import Swordsman from './Swordsman';
 import Undead from './Undead';
 import Zombie from './Zombie';
+/* eslint-disable no-unused-vars */
